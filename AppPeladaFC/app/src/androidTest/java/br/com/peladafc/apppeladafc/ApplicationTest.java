81c6cc13d26a5dd73f340c18b203eb19790a6538
Renan Junior <renan2junior@gmail.com>
2016-07-15 15:47:19 -0300

    Criação do projeto.

---
+++ AppPeladaFC/app/src/androidTest/java/br/com/peladafc/apppeladafc/ApplicationTest.java
@@ -0,0 +1,1 @@
+package br.com.peladafc.apppeladafc;import android.app.Application;import android.test.ApplicationTestCase;/** * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a> */public class ApplicationTest extends ApplicationTestCase<Application> {    public ApplicationTest() {        super(Application.class);    }}
